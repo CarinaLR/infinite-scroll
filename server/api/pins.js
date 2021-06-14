4788ad7a11d82eb81f71e388d8b6e8013e9bb39a
CarinaLR <carinalr84@gmail.com>
2021-06-14 09:15:41 -0400

    parse data

--- server/api/pins.js
+++ server/api/pins.js
@@ -7,7 +7,8 @@ const data = fs.readFileSync("nyc_ttp_pins.json", { encoding: "utf-8" });
 router.get("/", async (req, res, next) => {
   try {
     console.log("I'm here!");
-    res.status(200).send(data);
+    let jsonObj = JSON.parse(data);
+    res.status(200).send(jsonObj);
   } catch (error) {
     next(error);
   }
