f62574edbaa3cffdecb152703abc65b896828ba5
CarinaLR <carinalr84@gmail.com>
2021-06-14 09:55:30 -0400

    clena code

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const bodyParser = require("body-parser");
-// const fs = require("fs");
-
-// const data = fs.readFileSync("nyc_ttp_pins.json", { encoding: "utf-8" });
 
 app.use(bodyParser.json());
 
