23f082707dc0318b613fcf7608170983bb5859f9
CarinaLR <carinalr84@gmail.com>
2021-06-15 10:10:56 -0400

    calling cors

--- infinite-scroll/src/Layout/Home.js
+++ infinite-scroll/src/Layout/Home.js
@@ -1,36 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from "axios";
 
-export class Home extends Component {
-  loading = () => (
-    <div className="animated fadeIn pt-1 text-center">Loading...</div>
-  );
-  render() {
-    return (
-      <div>
-        <div id="content-wrapper" class="d-flex flex-column">
-          <div id="container text-center mb-4">
-            <div class="row">
-              <div class="col-lg-3 col-md-6 mb-4">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="assets/img/blackLogo.png"
-                    alt="..."
-                  ></img>
-                  <div class="card-body">
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                  </div>
+const Home = (props) => {
+  // state in our functional component, with React hooks
+  const [pins, setPins] = React.useState([]);
+
+  const url = "http://localhost:8080/api";
+
+  const getData = async () => {
+    const { data } = await axios.get(url);
+    setPins(data);
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  console.log("data =>", props);
+
+  return (
+    <div>
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="container text-center mb-4">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4">
+              <div class="card">
+                <img
+                  class="card-img-top"
+                  src="assets/img/blackLogo.png"
+                  alt="..."
+                ></img>
+                <div class="card-body">
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 8080;
 const bodyParser = require("body-parser");
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
-app.use("/", require("./server/api"));
+app.use("/api", require("./server/api"));
 
 app.get("/", (req, res) => res.send("HELLO PINTEREST"));
 
