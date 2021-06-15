9ae89fc04d71833aeb44c5098ea4ba3f13e1dca6
CarinaLR <carinalr84@gmail.com>
2021-06-15 12:58:31 -0400

    working on infinite-scroll componnet

--- infinite-scroll/src/Layout/Home.js
+++ infinite-scroll/src/Layout/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Home = (props) => {
   // state in our functional component, with React hooks
   const [pins, setPins] = React.useState([]);
 
-  const url = "http://localhost:8080/api";
-
   const getData = async () => {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get("/api");
+    console.log("data =>", data);
     setPins(data);
   };
 
@@ -16,29 +16,33 @@ const Home = (props) => {
     getData();
   }, []);
 
-  console.log("data =>", props);
-
   return (
-    <div>
-      <div id="content-wrapper" className="d-flex flex-column">
-        <div id="container text-center mb-4">
-          <div className="row">
-            <div className="col-lg-3 col-md-6 mb-4">
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src="assets/img/blackLogo.png"
-                  alt="..."
-                ></img>
-                <div className="card-body">
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
+    <div className="d-flex flex-column">
+      <div id="container text-center mb-4">
+        <div className="row">
+          <InfiniteScroll
+            dataLength={pins.length}
+            next={getData}
+            hasMore={true}
+            loader={<h3>Loading...</h3>}
+          >
+            {pins.map((pin) => (
+              <div className="col-md-6 mb-4" key={pin.id}>
+                <div className="card" key={pin.id}>
+                  <img
+                    className="card-img-top img-size-card"
+                    src={pin.images.orig}
+                    alt="..."
+                  ></img>
+                  <div classNAme="card-body">
+                    <h4>{pin.board.name}</h4>
+                    <p className="card-text">{pin.description}</p>
+                  </div>
+                  <label className="card-text">{pin.pinner.full_name}</label>
                 </div>
               </div>
-            </div>
-          </div>
+            ))}
+          </InfiniteScroll>
         </div>
       </div>
     </div>
